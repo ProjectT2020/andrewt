020ec8f7a44fe1eb6a642b6eb0d6ea79a64e0f97
andrewT <andrew@t.com>
2021-02-16 09:27:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header msg="Hello Header" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Andrew T' Website, I can change on the fly"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+        <nav>
+            <ul>
+                <li id="current"><a href="index.html">Home</a></li>
+                <li id="nav-blog"><a href="blog/index.html">Blog</a></li>
+                <li id="nav-ip"><a href="http://8.131.95.69:3000/foo/bar">你的IP</a></li>
+                <li id="nav-distance"><a href="distance.html">我们的距离</a></li>
+                <li id="nav-history"><a href="./2021年2月12日index.html">往期主页</a></li>
+                <li id="nav-about"><a href="about.html">About</a></li>
+            </ul>
+        </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
